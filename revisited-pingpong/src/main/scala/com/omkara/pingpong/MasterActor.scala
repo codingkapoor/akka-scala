7455e8a622a34b190c44e423712855eaaccf7e2c
m0dernm0nk <mail@shivamkapoor.com>
2016-05-21 01:21:00 +0530

    MasterActor now supervision it's children actors as per AllForOne supervision strategy

--- revisited-pingpong/src/main/scala/com/omkara/pingpong/MasterActor.scala
+++ revisited-pingpong/src/main/scala/com/omkara/pingpong/MasterActor.scala
@@ -1,6 +1,8 @@
 package com.omkara.pingpong
 
 import akka.actor.{ Actor, ActorLogging, ActorRef, Props }
+import akka.actor.{ AllForOneStrategy, ActorKilledException, ActorInitializationException }
+import akka.actor.SupervisorStrategy._
 
 class MasterActor extends Actor with ActorLogging {
   import MasterActor._
@@ -16,13 +18,13 @@ class MasterActor extends Actor with ActorLogging {
     case InitializeSys => {
       log.info("MasterActor is instantiating a RouterActor instance.")
       router = context.actorOf(routerActorProps, "router-actor")
-      
+
       log.info("MasterActor is instantiating PingPongSupervisorActor.")
       supervisor = context.actorOf(pingPongSupervisorActorProps(router), "pingpong-supervisor-actor")
       supervisor ! StartPingPongActors(5)
-      
+
       Thread sleep 1000
-      
+
       log.info("MasterActor is asking RouterActor to assign roles.")
       router ! AssignRoles
     }
@@ -30,14 +32,21 @@ class MasterActor extends Actor with ActorLogging {
     case TerminateSys => {
       log.info("MasterActor is initiating actor system termination.")
       router ! Conclude
-      
+
       Thread sleep 1000
-      
+
       log.info("MasterActor is terminating actor system gracefully.")
       context.system.shutdown()
-      
+
     }
   }
+
+  override val supervisorStrategy = AllForOneStrategy() {
+    case _: ActorInitializationException => Stop
+    case _: ActorKilledException         => Stop
+    case _: Exception                    => Restart
+  }
+
 }
 
 object MasterActor {
